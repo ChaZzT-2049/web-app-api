9a8efb07ec6cb7a3c0c684672b6d9fc745c7c077
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-11-20 16:55:45 -0500

    adding api to web app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
-  return (
-    <div>
-      Hola mundo
-    </div>
-  );
+  const [data, setData] = useState('');
+  useEffect(() => {
+    (async function () {
+      const body = await( await fetch(`/api/message`)).text();
+      setData(body);
+    })();
+  });
+
+  return <div>
+    <h1>Hola mundo, <br/> {data}</h1>  
+  </div>
 }
 
 export default App;
